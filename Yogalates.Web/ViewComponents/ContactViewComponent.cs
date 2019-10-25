4ca4ac3e3f70f7e21e9f79a7953b0f7d3703ed9b
Google Google <Google@students.softuni.bg>
2019-10-25 13:51:25 +0300

    Added pointer to contact

---
+++ Yogalates.Web/ViewComponents/ContactViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Yogalates.Services.Contracts;
+
+namespace Yogalates.Web.ViewComponents
+{
+    public class ContactViewComponent : ViewComponent
+    {
+        private readonly IBlogService _contact;
+
+        public ContactViewComponent(IBlogService contact)
+        {
+            _contact = contact;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            //var model = await _contact.GetContact;
+            return View(/*model*/);
+        }
+    }
+}

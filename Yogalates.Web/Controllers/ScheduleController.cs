9fb66a755b8c9e594f5ea9bc7a319147e9423edb
Google Google <Google@students.softuni.bg>
2019-10-26 19:48:04 +0300

    Page About ready

---
+++ Yogalates.Web/Controllers/ScheduleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yogalates.Web.Controllers
+{
+    public class ScheduleController : BaseController
+    {
+        public IActionResult Schedule()
+        {
+            return View();
+        }
+    }
+}

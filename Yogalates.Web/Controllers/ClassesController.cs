a17973570367da66c1ad6e5c5d873585fcc5c2d5
Google Google <Google@students.softuni.bg>
2019-10-26 11:18:01 +0300

    Added main images in main page

---
+++ Yogalates.Web/Controllers/ClassesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yogalates.Web.Controllers
+{
+    public class ClassesController : BaseController
+    {
+        public IActionResult Classes()
+        {
+            return View();
+        }
+    }
+}

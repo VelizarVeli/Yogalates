cd6cf7a69d0e69023cd49100fc9baa41bcb5c0af
Google Google <Google@students.softuni.bg>
2019-10-26 23:38:48 +0300

    Events view added

---
+++ Yogalates.Web/Controllers/EventsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Yogalates.Web.Controllers
+{
+    public class EventsController : BaseController
+    {
+        public IActionResult Events()
+        {
+            return View();
+        }
+    }
+}

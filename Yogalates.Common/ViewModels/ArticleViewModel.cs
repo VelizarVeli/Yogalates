d86a4ccaadeb700482524e2854fb5b24d5b4abba
Google Google <Google@students.softuni.bg>
2019-12-04 19:39:40 +0200

    Minor changes

--- Yogalates.Common/ViewModels/ArticleViewModel.cs
+++ Yogalates.Common/ViewModels/ArticleViewModel.cs
@@ -11,7 +11,7 @@ namespace Yogalates.Common.ViewModels
         public string Image800X533 { get; set; }
 
         public string Name { get; set; }
-        
+
         public string Content { get; set; }
     }
 }
